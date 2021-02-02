ffd83f2387805ad6b2620af61717d88ce424031c
Parth Malani <pmalani@deloitte.com>
2021-02-03 01:40:50 +0530

    databinding

--- app/src/main/java/com/example/animals/view/LoginActivity.kt
+++ app/src/main/java/com/example/animals/view/LoginActivity.kt
@@ -12,14 +12,14 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+
+    val user = User("test","test" )
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val user = User("test","test" )
-
         val login_btn: Button = findViewById(R.id.login_btn)
-
 
         login_btn.setOnClickListener(View.OnClickListener {
             var userInput : TextInputEditText = findViewById(R.id.username_et)
@@ -27,17 +27,19 @@ class LoginActivity : AppCompatActivity() {
 
             var passInput : TextInputEditText = findViewById(R.id.password_et)
             var pass : String = passInput.text.toString()
-
-            if(userId == user.user_id && pass == user.password)
-            {
-                val intent : Intent = Intent(this, HomePage::class.java)
-                startActivity(intent)
-            }
-            else{
-                Toast.makeText(baseContext, "Incorrect username or password", Toast.LENGTH_SHORT).show()
-
-            }
+            checkUser(userId, pass)
         })
 
+    }
+    fun checkUser(userId : String, pass : String){
+        if(userId == user.user_id && pass == user.password)
+        {
+            val intent : Intent = Intent(this, HomePage::class.java)
+            startActivity(intent)
+        }
+        else{
+            Toast.makeText(baseContext, "Incorrect username or password", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
